9d52bb3d5285bf99355f73c11304c7b1940ca294
Phasolka0 <100365746+Phasolka0@users.noreply.github.com>
2023-10-10 01:48:26 +0300

    cache integration 0.2.4

--- build/entities/pool.d.ts
+++ build/entities/pool.d.ts
@@ -79,12 +79,18 @@ export declare class Pool {
      * @returns The output amount and the pool with updated state
      */
     getOutputAmountOptimized(inputAmount: CurrencyAmount<Token>, sqrtPriceLimitX64?: JSBI): CurrencyAmount<Token>;
+    /**
+     * Given an input amount of a token, return the computed output amount, and a pool with state updated after the trade
+     * @param inputAmount The input amount for which to quote the output amount
+     * @param sqrtPriceLimitX64 The Q64.96 sqrt price limit
+     * @returns The output amount and the pool with updated state
+     */
     getOutputAmountOptimizedWithCache(inputAmount: CurrencyAmount<Token>, sqrtPriceLimitX64?: JSBI): CurrencyAmount<Token>;
     /**
-     * Given a desired output amount of a token, return the computed input amount and a pool with state updated after the trade
-     * @param outputAmount the output amount for which to quote the input amount
-     * @param sqrtPriceLimitX64 The Q64.96 sqrt price limit. If zero for one, the price cannot be less than this value after the swap. If one for zero, the price cannot be greater than this value after the swap
-     * @returns The input amount and the pool with updated state
+     * Given an input amount of a token, return the computed output amount, and a pool with state updated after the trade
+     * @param inputAmount The input amount for which to quote the output amount
+     * @param sqrtPriceLimitX64 The Q64.96 sqrt price limit
+     * @returns The output amount and the pool with updated state
      */
     getInputAmount(outputAmount: CurrencyAmount<Token>, sqrtPriceLimitX64?: JSBI): [CurrencyAmount<Token>, Pool];
     getInputAmountOptimized(outputAmount: CurrencyAmount<Token>, sqrtPriceLimitX64?: JSBI): CurrencyAmount<Token>;
